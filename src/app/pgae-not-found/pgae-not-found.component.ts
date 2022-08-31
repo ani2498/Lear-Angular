d98f471e7b44b36e941899581bed167bf0487c08
ANIKET MODKER <aniketmodker24@gmail.com>
2022-08-31 11:33:42 +0530

    init

--- src/app/pgae-not-found/pgae-not-found.component.ts
+++ src/app/pgae-not-found/pgae-not-found.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pgae-not-found',
   templateUrl: './pgae-not-found.component.html',
-  styleUrls: ['./pgae-not-found.component.css']
+  styleUrls: ['./pgae-not-found.component.css'],
 })
 export class PgaeNotFoundComponent implements OnInit {
+  bookUrl =
+    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onConfirm() {
+    let res = confirm('Are you sure ');
+
+    if (res) {
+      console.log('yes');
+    } else {
+      console.log('No');
+    }
   }
-
 }
